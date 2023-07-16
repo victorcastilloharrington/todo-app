749c42d12c812d828143ce9cf5e6fd0684c83605
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2023-07-16 13:44:46 -0300

    updated api method logic

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -6,22 +6,26 @@ const todos: NextApiHandler = async (req, res) => {
   // Gets the `id` from the query field.
   const { id } = req.query;
 
-  // Handles an API GET
-  if (req.method === "GET") {
-    // HTTP GET `/api/example?id=100`: 200 OK
-    const now = await db.query("SELECT NOW()");
-    res.status(200).json({
-      success: true,
-      id,
-      now,
-      message: "hi there!",
-    });
-  } else {
-    // Handles all other HTTP methods with a 404
-    // HTTP POST `/api/example`: 404 ERROR
-    res.status(404).end();
-  }
-};
+  switch (req.method) {
+    // Handles an API GET
+    case 'GET':
+      // HTTP GET `/api/example?id=100`: 200 OK
+      const now = await db.query("SELECT NOW()");
+      res.status(200).json({
+        success: true,
+        id,
+        now,
+        message: "hi there!",
+      });
+      break;
+    default:
+      // Handles all other HTTP methods with a 404
+      // HTTP POST `/api/example`: 404 ERROR
+      res.status(404).end();
+      break;
+
+  };
+
+}
 
 export default todos;
-
